da39fb415a33b7c6be4145f1331db468faa98445
Sahir Maredia <sahirzm@gmail.com>
2023-07-25 23:41:33 -0400

    Replace evil mode with meow

--- init.el
+++ init.el
@@ -11,6 +11,8 @@
 
 ;; set firacode font
 (set-face-attribute 'default nil :font "FiraCode Nerd Font")
+(add-to-list 'default-frame-alist
+             '(font . "FiraCode Nerd Font-10"))
 
 ;; Display line numbers in all buffers
 (global-display-line-numbers-mode 1)
@@ -26,7 +28,6 @@
 
 ;;treemacs
 (use-package treemacs
-  :ensure t
   :defer t)
 
 ;; make emacs write custom configuration to separate file
@@ -38,7 +39,6 @@
 (use-package nerd-icons)
 ;; doom themes
 (use-package doom-themes
-  :ensure t
   :after (all-the-icons nerd-icons)
   :config
   ;; Global settings (defaults)
@@ -58,7 +58,6 @@
 
 ;; modeline
 (use-package doom-modeline
-  :ensure t
   :after (doom-themes)
   :init (doom-modeline-mode 1))
 
@@ -146,19 +145,108 @@
 (setq select-enable-clipboard t)
 
 ;; evil mode
-(use-package evil
-  :ensure t
-  :init
-  (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
-  (setq evil-want-keybinding nil)
-  :config
-  (evil-mode 1))
+;;(use-package evil
+;;  :init
+;;  (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
+;;  (setq evil-want-keybinding nil)
+;;  :config
+;;  (evil-mode 1))
+;;
+;;(use-package evil-collection
+;;  :after evil
+;;  :config
+;;  (evil-collection-init))
 
-(use-package evil-collection
-  :after evil
-  :ensure t
+;; modal editing using meow
+(defun meow-setup ()
+  (setq meow-cheatsheet-layout meow-cheatsheet-layout-qwerty)
+  (meow-motion-overwrite-define-key
+   '("j" . meow-next)
+   '("k" . meow-prev)
+   '("<escape>" . ignore))
+  (meow-leader-define-key
+   ;; SPC j/k will run the original command in MOTION state.
+   '("j" . "H-j")
+   '("k" . "H-k")
+   ;; Use SPC (0-9) for digit arguments.
+   '("1" . meow-digit-argument)
+   '("2" . meow-digit-argument)
+   '("3" . meow-digit-argument)
+   '("4" . meow-digit-argument)
+   '("5" . meow-digit-argument)
+   '("6" . meow-digit-argument)
+   '("7" . meow-digit-argument)
+   '("8" . meow-digit-argument)
+   '("9" . meow-digit-argument)
+   '("0" . meow-digit-argument)
+   '("/" . meow-keypad-describe-key)
+   '("?" . meow-cheatsheet))
+  (meow-normal-define-key
+   '("0" . meow-expand-0)
+   '("9" . meow-expand-9)
+   '("8" . meow-expand-8)
+   '("7" . meow-expand-7)
+   '("6" . meow-expand-6)
+   '("5" . meow-expand-5)
+   '("4" . meow-expand-4)
+   '("3" . meow-expand-3)
+   '("2" . meow-expand-2)
+   '("1" . meow-expand-1)
+   '("-" . negative-argument)
+   '(";" . meow-reverse)
+   '("," . meow-inner-of-thing)
+   '("." . meow-bounds-of-thing)
+   '("[" . meow-beginning-of-thing)
+   '("]" . meow-end-of-thing)
+   '("a" . meow-append)
+   '("A" . meow-open-below)
+   '("b" . meow-back-word)
+   '("B" . meow-back-symbol)
+   '("c" . meow-change)
+   '("d" . meow-delete)
+   '("D" . meow-backward-delete)
+   '("e" . meow-next-word)
+   '("E" . meow-next-symbol)
+   '("f" . meow-find)
+   '("g" . meow-cancel-selection)
+   '("G" . meow-grab)
+   '("h" . meow-left)
+   '("H" . meow-left-expand)
+   '("i" . meow-insert)
+   '("I" . meow-open-above)
+   '("j" . meow-next)
+   '("J" . meow-next-expand)
+   '("k" . meow-prev)
+   '("K" . meow-prev-expand)
+   '("l" . meow-right)
+   '("L" . meow-right-expand)
+   '("m" . meow-join)
+   '("n" . meow-search)
+   '("o" . meow-block)
+   '("O" . meow-to-block)
+   '("p" . meow-yank)
+   '("q" . meow-quit)
+   '("Q" . meow-goto-line)
+   '("r" . meow-replace)
+   '("R" . meow-swap-grab)
+   '("s" . meow-kill)
+   '("t" . meow-till)
+   '("u" . meow-undo)
+   '("U" . meow-undo-in-selection)
+   '("v" . meow-visit)
+   '("w" . meow-mark-word)
+   '("W" . meow-mark-symbol)
+   '("x" . meow-line)
+   '("X" . meow-goto-line)
+   '("y" . meow-save)
+   '("Y" . meow-sync-grab)
+   '("z" . meow-pop-selection)
+   '("'" . repeat)
+   '("<escape>" . ignore)))
+(use-package meow
   :config
-  (evil-collection-init))
+  (meow-setup)
+  (meow-global-mode 1))
 
 ;; Projectile
 (use-package projectile
@@ -447,8 +535,7 @@
 	lsp-javascript-format-enable nil))
 
 ;; nix LSP
-(use-package lsp-mode
-  :ensure t)
+(use-package lsp-mode)
 
 (use-package lsp-nix
   :ensure lsp-mode
@@ -458,8 +545,7 @@
   (lsp-nix-nil-formatter ["nixpkgs-fmt"]))
 
 (use-package nix-mode
-  :hook (nix-mode . lsp-deferred)
-  :ensure t)
+  :hook (nix-mode . lsp-deferred))
 
 ;; format code
 (use-package format-all
